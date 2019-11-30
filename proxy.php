81b47de973e4bbeaefcad39fa5ad7343be0f4c05
herbetom <git@herbetom.de>
2019-11-30 21:29:41 +0100

    use curl instead of get_url_contents This workaround is necessary because get_url_contents wouldn't stop on the webserver

--- proxy.php
+++ proxy.php
@@ -4,17 +4,21 @@
 // +  AutorIn:  Lukas Bisdorf
 // +  Beschreibung: Wraps the nodes.json to work around the CORS problem+
 // +  with ajax requests.
-// +  KorrektorIn:
+// +  KorrektorIn: Tom Herbers
 // +  Status:
-// +  Revision: 2018-05-17
+// +  Revision: 2019-11-30
 // +---------------------------------------------------------------------------
 
-
-$file = file_get_contents("https://map.ffrn.de/data/meshviewer.json");
-if (!$file){
-	$file = "{}";
+function get_url_contents($url) {
+    $crl = curl_init();
+    curl_setopt($crl, CURLOPT_URL, $url);
+    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);   // seconds before timeout
+    $ret = curl_exec($crl);
+    curl_close($crl);
+    return $ret;
 }
-
+$file = get_url_contents("https://map.ffrn.de/data/meshviewer.json");
 header('Content-Type: application/json');
 echo $file;
 
